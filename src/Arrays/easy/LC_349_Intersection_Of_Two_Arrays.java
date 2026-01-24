51a8b5cee636679014891addcb3feaf2e1591045
Shreyan Dhar <dharshreyan05@gmail.com>
2026-01-24 23:51:30 +0530

    24/01/26

---
+++ src/Arrays/easy/LC_349_Intersection_Of_Two_Arrays.java
@@ -0,0 +1,47 @@
+package Arrays.easy;
+//Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+//Example 1:
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2]
+
+//Example 2:
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [9,4]
+//Explanation: [4,9] is also accepted.
+
+//Constraints:
+//
+//        1 <= nums1.length, nums2.length <= 1000
+//        0 <= nums1[i], nums2[i] <= 1000
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC_349_Intersection_Of_Two_Arrays {
+    public static void main(String[] args) {
+        int [] nums1 = {1,2,2,1};
+        int [] nums2 = {2,2};
+        int [] output = intersection(nums1, nums2);
+        System.out.println(Arrays.toString(output));
+    }
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        int [] small = n1 > n2 ? nums2 : nums1;
+        int [] big = n1 > n2 ? nums1 : nums2;
+        List<Integer> myList = new ArrayList<>();
+        for(int i = 0; i < small.length; i++){
+            for(int j = 0; j < big.length; j++){
+                if(small[i] == big[j] && !myList.contains(small[i])){
+                    myList.add(small[i]);
+                    break;
+                }
+            }
+        }
+        return myList.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
