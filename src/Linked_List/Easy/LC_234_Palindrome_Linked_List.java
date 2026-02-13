0764916c8519923df3400f28afd21e5116a737cf
Shreyan Dhar <dharshreyan05@gmail.com>
2026-02-13 23:29:39 +0530

    13/02/26

---
+++ src/Linked_List/Easy/LC_234_Palindrome_Linked_List.java
@@ -0,0 +1,62 @@
+package Linked_List.Easy;
+
+public class LC_234_Palindrome_Linked_List {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+
+        ListNode secondHalf = reverse(slow);
+
+        ListNode firstHalf = head;
+        ListNode temp = secondHalf;
+
+        while (temp != null) {
+            if (firstHalf.val != temp.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        return prev;
+    }
+}
